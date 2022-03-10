65fb61966eced9defd00fdfcb677e01816c41c5e
Dean Peyman <dean.p@gso.schule.koeln>
2022-03-10 11:45:45 +0100

    Projektdateien hinzufügen.

---
+++ Arrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = 21;
+            int summe = 0;
+            int[] noten = new int[n];
+            for (int i = 1; i < n; i++) 
+            {
+
+                Console.WriteLine($"Bitte geben Sie die {i} Note ein");
+                noten[i] = Convert.ToInt32(Console.ReadLine());
+                summe = summe + noten[i];
+            }
+
+            for (int p = 0; p < noten.Length; p++)
+            {
+
+                Console.WriteLine(noten[p]);
+
+            }
+
+            Console.WriteLine("Der Notendurchschnitt beträgt:");
+            Console.WriteLine(summe / n);
+
+        }
+    }
+}
